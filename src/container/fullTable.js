c344a7233ccb790ba92c115632c503a78d7af2c6
AvenMerk <kremnevaaj@gmail.com>
2019-02-19 18:40:42 +0300

    add pagination

--- src/container/fullTable.js
+++ src/container/fullTable.js
@@ -2,25 +2,42 @@ import React from 'react';
 import { fetchFullData } from '../action/index';
 import { connect } from 'react-redux';
 import TableStroke from "../components/tableStroke";
+import Button from "../components/button";
 
 class FullTable extends React.Component {
 
     state = {
         currentPage: 0,
         showFields: 50,
+        pages: [],
     };
 
     componentDidMount() {
         const { dispatch } = this.props;
         dispatch(fetchFullData());
+
     }
+
+    //TODO сделать расчет страниц при выборе большого объема данных
+    setNumberOfPages = () => {
+        const numberOfPages = this.props.fullData.length / this.state.showFields;
+        console.log(numberOfPages);
+        let pages = [];
+        for (let i = 1; i <= numberOfPages; i++) {
+            pages.push(i);
+        }
+        this.setState({pages: pages});
+    };
+
+    changePage = () => {
+        this.setState({currentPage: this.state.currentPage + 1});
+    };
 
     mapFullData = (fullData) => {
         const { currentPage, showFields } = this.state;
         const beginning = currentPage*showFields;
         const end = (currentPage + 1)*showFields;
         const newData = fullData.slice(beginning, end);
-        console.log(newData);
         return newData.map((data, index) =>
             <TableStroke key={index} data={data}/>
         );
@@ -36,6 +53,11 @@ class FullTable extends React.Component {
                         Loading...</h2> : <h2>Empty.</h2>)
                 : <div style={{opacity: isFetching ? 0.5 : 1}}>
                     <h2>Table</h2>
+                    <Button name="Create array" onClick={this.setNumberOfPages}/>
+                    {this.state.pages.map((page, index) =>
+                    <Button name={page} onClick={this.changePage} key={index}/>
+                    )}
+
                     {this.mapFullData(fullData)}
                 </div>
             }
