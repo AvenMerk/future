7b6d4e9dca9d5ba27390f7c183f1b378ee22d5ce
AvenMerk <kremnevaaj@gmail.com>
2019-02-19 16:12:14 +0300

    add action and reducer for full info

--- src/action/index.js
+++ src/action/index.js
@@ -1,5 +1,15 @@
 export const REQUEST_SMALL = 'REQUEST_SMALL';
 export const RECEIVE_SMALL = 'RECEIVE_SMALL';
+export const REQUEST_FULL = 'REQUEST_FULL';
+export const RECEIVE_FULL = 'RECEIVE_FULL';
+export const SELECT_MODE = 'SELECT_MODE';
+
+export const selectMode = mode => {
+    return {
+        type: SELECT_MODE,
+        mode
+    }
+};
 
 export const requestSmall = () => ({
     type: REQUEST_SMALL
@@ -19,4 +29,24 @@ export const fetchSmallData = () => (dispatch) => {
             error => console.log("Something went wrong", error)
         )
         .then(json => dispatch(receiveSmall(json)))
+};
+
+export const requestFull = () => ({
+    type: REQUEST_FULL
+});
+
+export const receiveFull = (json) => ({
+    type: RECEIVE_FULL,
+    fullData: json,
+    receivedAt: Date.now()
+});
+
+export const fetchFullData = () => (dispatch) => {
+    dispatch(requestFull());
+    return fetch(`http://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D`)
+        .then(
+            response => response.json(),
+            error => console.log("Something went wrong", error)
+        )
+        .then(json => dispatch(receiveFull(json)))
 };
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,4 +1,5 @@
 import {RECEIVE_SMALL, REQUEST_SMALL} from "../action/index";
+import {RECEIVE_FULL, REQUEST_FULL} from "../action/index";
 import { combineReducers } from 'redux';
 
 const smallReducer = (state = {isFetching: false, smallData: []}, action) => {
@@ -20,7 +21,26 @@ const smallReducer = (state = {isFetching: false, smallData: []}, action) => {
     }
 };
 
+const fullReducer = (state = {isFetching: false, fullData: []}, action) => {
+    switch (action.type) {
+        case REQUEST_FULL:
+            return {
+                ...state,
+                isFetching: true
+            };
+        case RECEIVE_FULL:
+            return {
+                ...state,
+                isFetching: false,
+                fullData: action.fullData,
+                lastUpdated: action.receivedAt
+            };
+        default:
+            return state
+    }
+};
 
 export default combineReducers({
     smallReducer,
+    fullReducer,
 })
